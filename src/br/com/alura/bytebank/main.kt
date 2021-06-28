07ea9778b3bee4bd73803d1f56933f36c1e380c0
MalkonF <sudo777@protonmail.com>
2021-06-28 17:38:14 -0300

    Lançando uma exception para ver qual o comportamento da pilha de execução

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,14 +1,15 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.teste.testaAny
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
     println("início main")
+    10/0
     funcao1()
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
     funcao2()
     println("fim funcao1")
@@ -16,8 +17,10 @@ fun funcao1(){
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
+    for (i in 1..5) {
         println(i)
+        val endereco = Any()
+        endereco as Endereco //se na JVM não tiver alguma classe que trate essa exceção ela vai estourar no console
     }
     println("fim funcao2")
 }
