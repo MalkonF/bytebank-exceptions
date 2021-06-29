6a1c121a2fdccb7888651bb0af62166c79c034a3
MalkonF <sudo777@protonmail.com>
2021-06-28 22:39:37 -0300

    Personalizando msg de exceções / Lidando com mais de uma exception

--- src/br/com/alura/bytebank/exception/SaldoInsuficienteException.kt
+++ src/br/com/alura/bytebank/exception/SaldoInsuficienteException.kt
@@ -1,4 +1,6 @@
 package br.com.alura.bytebank.exception
 
 //aqui nao é possível fazer Exception(message = "Saldo insuficiente") porque é uma classe herdada de Java
-class SaldoInsuficienteException : Exception("O saldo é insuficiente")
+class SaldoInsuficienteException(
+    mensagem: String = "O saldo é insuficiente"
+) : Exception(mensagem)
--- src/br/com/alura/bytebank/modelo/Conta.kt
+++ src/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,11 +1,12 @@
 package br.com.alura.bytebank.modelo
 
+import br.com.alura.bytebank.exception.FalhaAutenticacaoException
 import br.com.alura.bytebank.exception.SaldoInsuficienteException
 
 abstract class Conta(
     var titular: Cliente,
     val numero: Int
-) {
+) : Autenticavel { // aqui classes filhas ficam obrigadas a implementarem metodo de Autenticável se está classe Conta não implementar
     var saldo = 0.0
         protected set
 
@@ -19,6 +20,10 @@ abstract class Conta(
         total++
     }
 
+    override fun autentica(senha: Int): Boolean {
+        return titular.autentica(senha)//usa a implementação que tá em Cliente
+    }
+
     fun deposita(valor: Double) {
         if (valor > 0) {
             this.saldo += valor
@@ -27,14 +32,19 @@ abstract class Conta(
 
     abstract fun saca(valor: Double)
 
-    fun transfere(valor: Double, destino: Conta) {
+    fun transfere(valor: Double, destino: Conta, senha: Int) {
         if (saldo < valor)
-            throw SaldoInsuficienteException()
+            throw SaldoInsuficienteException(mensagem = "O saldo de ${saldo} é insuficiente para o valor requerido ${valor}")
         saldo -= valor
         destino.deposita(valor)
-    }
-}
 
+        if (!autentica(senha)) {
+            throw FalhaAutenticacaoException()
+        }
+    }
+
+
+}
 
 class ContaCorrente(
     titular: Cliente,
