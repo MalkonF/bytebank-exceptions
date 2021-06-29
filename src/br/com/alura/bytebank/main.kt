9ce56f8d6b1cd636d89c3d1cee4af650218ca411
MalkonF <sudo777@protonmail.com>
2021-06-29 11:35:32 -0300

    Outros recursos do null safety: elvis operator etc

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -4,15 +4,28 @@ import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
     var enderecoNulo: Endereco? = null // ?(safe call) permite receber nulo
-    print(enderecoNulo?.logradouro)
-    var enderecoNulo2: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    println("t" + enderecoNulo?.logradouro)
+    var enderecoNulo2: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "predio")
     var logradouroNovo: String? = enderecoNulo?.logradouro
-    println(enderecoNulo?.logradouro?.length?.toUByte())//sempre para acessar tem que usar o safe call
+    println("e" + enderecoNulo?.logradouro?.length?.toUByte())//sempre para acessar tem que usar o safe call
     //porque a var enderecoNulo foi decalradac om ? e null
     //Só quando vc usa o let vc pode usar sem safe call uma declaração que foi feita com safe call
-    enderecoNulo?.let {
-        println(it.logradouro.length)
+    enderecoNulo2?.let {
+        println("ex" + it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: 0 //elvis operator devolve 0 caso seja vazio ou o length caso n seja nulo
+        println(tamanhoComplemento)
     }
+
+    teste(1)
+}
+
+//se der erro na conversão, por exemplo, se colocar string no lugar de int em teste() ele vai atribuir nulo
+//ao invés de dar exception
+fun teste(valor: Any) {
+    val numero: Int? = valor as? Int
+    print(numero)
+
 }
 
 
